16d21ec65cbf26c9fbcd981818a819c74aea8899
kawjaw66 <kawjaw66@gmail.com>
2021-06-28 20:17:36 +0700

    ver 2

--- worker.js
+++ worker.js
@@ -4,6 +4,7 @@ module.exports=async function process_to_grader(req,res){
     if(!req.body){
         return res.json({problem: 'json_incomplete'})
     }
+    console.log(req.body)
     const resultAfterCompile =await checkResult(req.body.code,req.body.input,req.body.output);
     const result_toback = {
         questionId : req.body.questionId,
